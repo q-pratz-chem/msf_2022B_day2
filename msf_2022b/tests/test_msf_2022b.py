a60c44c731e4a6ff939cd5860d6ea2f51ded14d6
Pratiksha Gaikwad <pratiksha4399@gmail.com>
2022-07-26 14:13:27 -0400

    add tests

--- msf_2022b/tests/test_msf_2022b.py
+++ msf_2022b/tests/test_msf_2022b.py
@@ -5,9 +5,90 @@ Unit and regression test for the msf_2022b package.
 # Import package, test suite, and other packages as needed
 import sys
 
+import numpy as np
 import pytest
 
 import msf_2022b
+
+
+'''
+Test exercise:
+
+Write a test for the calculate_angle function,
+
+Use the points:
+
+    r1 = [0, 0, -1] 
+    r2 = [0, 0, 0] 
+    r3 = [1, 0, 0] 
+
+'''
+
+@pytest.fixture
+def methane_molecule():
+    symbols = np.array(["C", "H", "H", "H", "H"])
+    coordinates = np.array([
+        [1, 1, 1],
+        [2.4, 1, 1],
+        [-0.4, 1, 1],
+        [1,  1, 2.4],
+        [1,  1, -0.4]])
+
+    return symbols, coordinates 
+
+
+
+
+
+
+@pytest.mark.parametrize("p1, p2, p3, expected_angle", [
+    (np.array([0, 0, -1]), np.array([0, 0, 0]), np.array([1, 0, 0]), 90),
+    (np.array([np.sqrt(2)/2, np.sqrt(2)/2, 0]), np.array([0, 0, 0]), np.array([1, 0, 0]), 45)
+])
+
+
+
+def test_calculate_angle_many(p1, p2, p3, expected_angle):
+
+    calculated_angle = msf_2022b.calculate_angle(p1, p2, p3, degrees = True)
+ 
+    assert expected_angle == calculated_angle
+    
+
+def test_build_bond_distance_error(methane_molecule):
+
+    coordinates = methane_molecule[1]
+
+    with pytest.raises(ValueError):
+        msf_2022b.build_bond_list(coordinates, min_bond = -1)
+
+
+@pytest.mark.skip
+def test_calculate_angle():
+    '''Test that the calculate_angle function calculates what we expect.'''
+
+    r1 = np.array([0, 0, -1]) 
+    r2 = np.array([0, 0, 0])
+    r3 = np.array([1, 0, 0])
+
+    expected_output = 90.0
+
+    observed_output = msf_2022b.calculate_angle(r1, r2, r3, degrees=True)
+  
+    assert expected_output == observed_output
+
+
+def test_calculate_distance():
+    '''Test that the calculate_distance function calculates what we expect.'''
+
+    r1 = np.array([0, 0, 0])
+    r2 = np.array([0, 1, 0])
+
+    expected_output = 1.0
+
+    observed_output = msf_2022b.calculate_distance(r1, r2)
+  
+    assert expected_output == observed_output
 
 
 def test_msf_2022b_imported():
